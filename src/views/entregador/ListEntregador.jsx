4b4f7cd6bc1be9b251a5cd9be48a367d79b800cc
aluno <>
2024-07-04 16:06:41 -0300

    04072024

--- src/views/entregador/ListEntregador.jsx
+++ src/views/entregador/ListEntregador.jsx
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
+import { Button, Container, Divider, Icon, Table, Modal, Header } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
 export default function ListEntregador() {
 
     const [lista, setLista] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
+    const [idRemover, setIdRemover] = useState();
+
 
     useEffect(() => {
         carregarLista();
@@ -38,6 +41,30 @@ export default function ListEntregador() {
             return 'Não'
         }
     }
+
+    function confirmaRemover(id) {
+        setOpenModal(true)
+        setIdRemover(id)
+    }
+ 
+    async function remover() {
+
+        await axios.delete('http://localhost:8081/api/entregador/' + idRemover)
+        .then((response) => {
+  
+            console.log('Cliente removido com sucesso.')
+  
+            axios.get("http://localhost:8081/api/entregador")
+            .then((response) => {
+                setLista(response.data)
+            })
+        })
+        .catch((error) => {
+            console.log('Erro ao remover um entregador.')
+        })
+        setOpenModal(false)
+    }
+ 
 
     return (
         <div>
@@ -111,7 +138,8 @@ export default function ListEntregador() {
                                                 circular
                                                 color='red'
                                                 title='Clique aqui para remover este cliente'
-                                                icon>
+                                                icon
+                                                onClick={e => confirmaRemover(entregador.id)}>
                                                 <Icon name='trash' />
                                             </Button>
 
@@ -124,6 +152,26 @@ export default function ListEntregador() {
                     </div>
                 </Container>
             </div>
+
+            <Modal
+               basic
+               onClose={() => setOpenModal(false)}
+               onOpen={() => setOpenModal(true)}
+               open={openModal}
+         >
+               <Header icon>
+                   <Icon name='trash' />
+                   <div style={{marginTop: '5%'}}> Tem certeza que deseja remover esse registro? </div>
+               </Header>
+               <Modal.Actions>
+                   <Button basic color='red' inverted onClick={() => setOpenModal(false)}>
+                       <Icon name='remove' /> Não
+                   </Button>
+                   <Button color='green' inverted onClick={() => remover()}>
+                       <Icon name='checkmark' /> Sim
+                   </Button>
+               </Modal.Actions>
+         </Modal>
 
         </div>
     )
--- src/views/produto/ListProduto.jsx
+++ src/views/produto/ListProduto.jsx
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
+import { Button, Container, Divider, Icon, Table, Modal, Header } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
 export default function ListProduto() {
 
     const [lista, setLista] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
+    const [idRemover, setIdRemover] = useState();
+
 
     useEffect(() => {
         carregarLista();
@@ -17,8 +20,32 @@ export default function ListProduto() {
         axios.get("http://localhost:8081/api/produto")
         .then((response) => {
             setLista(response.data)
-        })
-} return (
+        })}
+
+        function confirmaRemover(id) {
+            setOpenModal(true)
+            setIdRemover(id)
+        }
+
+        async function remover() {
+
+            await axios.delete('http://localhost:8081/api/produto/' + idRemover)
+            .then((response) => {
+      
+                console.log('Cliente removido com sucesso.')
+      
+                axios.get("http://localhost:8081/api/produto")
+                .then((response) => {
+                    setLista(response.data)
+                })
+            })
+            .catch((error) => {
+                console.log('Erro ao remover um produto.')
+            })
+            setOpenModal(false)
+        }
+     
+ return (
 
         <div>
             <MenuSistema tela={'produto'} />
@@ -82,7 +109,8 @@ export default function ListProduto() {
                                                 circular
                                                 color='red'
                                                 title='Clique aqui para remover este cliente'
-                                                icon>
+                                                icon
+                                                onClick={e => confirmaRemover(produto.id)}>
                                                 <Icon name='trash' />
                                             </Button>
 
@@ -95,6 +123,25 @@ export default function ListProduto() {
                     </div>
                 </Container>
             </div>
+        	<Modal
+               basic
+               onClose={() => setOpenModal(false)}
+               onOpen={() => setOpenModal(true)}
+               open={openModal}
+         >
+               <Header icon>
+                   <Icon name='trash' />
+                   <div style={{marginTop: '5%'}}> Tem certeza que deseja remover esse registro? </div>
+               </Header>
+               <Modal.Actions>
+                   <Button basic color='red' inverted onClick={() => setOpenModal(false)}>
+                       <Icon name='remove' /> Não
+                   </Button>
+                   <Button color='green' inverted onClick={() => remover()}>
+                       <Icon name='checkmark' /> Sim
+                   </Button>
+               </Modal.Actions>
+         </Modal>
 
         </div>
 );
