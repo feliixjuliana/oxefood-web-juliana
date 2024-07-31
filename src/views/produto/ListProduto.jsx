ef66d87395c84ab4e9726d1ed71dfb48db238b94
aluno <aluno@ifpe>
2024-07-31 18:56:24 -0300

    PEGA PELO AMOR DE DEUS

--- src/views/produto/ListProduto.jsx
+++ src/views/produto/ListProduto.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table, Modal, Header } from 'semantic-ui-react';
+import { Button, Container, Divider, Icon, Table, Modal, Header, Form, Menu, Segment } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
 export default function ListProduto() {
@@ -9,6 +9,12 @@ export default function ListProduto() {
     const [lista, setLista] = useState([]);
     const [openModal, setOpenModal] = useState(false);
     const [idRemover, setIdRemover] = useState();
+    const [menuFiltro, setMenuFiltro] = useState();
+    const [codigo, setCodigo] = useState();
+    const [titulo, setTitulo] = useState();
+    const [idCategoria, setIdCategoria] = useState();
+    const [listaCategoriaProduto, setListaCategoriaProduto] = useState([]);
+
 
 
     useEffect(() => {
@@ -20,7 +26,71 @@ export default function ListProduto() {
         axios.get("http://localhost:8081/api/produto")
         .then((response) => {
             setLista(response.data)
-        })}
+        })
+        axios.get("http://localhost:8081/api/categoriaProduto")
+        .then((response) => {
+ 
+            const dropDownCategorias = [];
+            dropDownCategorias.push({ text: '', value: '' });
+            response.data.map(c => (
+                dropDownCategorias.push({ text: c.descricao, value: c.id })
+            ))
+ 
+            setListaCategoriaProduto(dropDownCategorias)
+         
+        })
+ 
+    
+    }
+
+       function handleMenuFiltro() {
+
+        if (menuFiltro === true) {
+            setMenuFiltro(false);
+        } else {
+            setMenuFiltro(true);
+        }
+    }
+
+    async function filtrarProdutos(codigoParam, tituloParam, idCategoriaParam) {
+
+        let formData = new FormData();
+ 
+        if (codigoParam !== undefined) {
+            setCodigo(codigoParam)
+            formData.append('codigo', codigoParam);
+        }
+        if (tituloParam !== undefined) {
+            setTitulo(tituloParam)
+            formData.append('titulo', tituloParam);
+        }
+        if (idCategoriaParam !== undefined) {
+            setIdCategoria(idCategoriaParam)
+            formData.append('idCategoria', idCategoriaParam);
+        }
+ 
+        await axios.post("http://localhost:8081/api/produto/filtrar", formData)
+        .then((response) => {
+            setLista(response.data)
+        })
+    }
+ 
+ 
+    function handleChangeCodigo(value) {
+ 
+        filtrarProdutos(value, titulo, idCategoria);
+    }
+ 
+    function handleChangeTitulo(value) {
+ 
+        filtrarProdutos(codigo, value, idCategoria);
+    }
+ 
+    function handleChangeCategoriaProduto(value) {
+ 
+        filtrarProdutos(codigo, titulo, value);
+    }
+ 
 
         function confirmaRemover(id) {
             setOpenModal(true)
@@ -57,6 +127,19 @@ export default function ListProduto() {
                     <Divider />
 
                     <div style={{ marginTop: '4%' }}>
+                    <Menu compact>
+                               <Menu.Item
+                                   name='menuFiltro'
+                                   active={menuFiltro === true}
+                                   onClick={() => handleMenuFiltro()}
+                               >
+                                   <Icon name='filter' />
+                                   Filtrar
+                               </Menu.Item>
+                           </Menu>
+...
+
+
                         <Button
                             label='Novo'
                             circular
@@ -66,6 +149,47 @@ export default function ListProduto() {
                             as={Link}
                             to='/form-produto'
                         />
+                            { menuFiltro ?
+                            
+                            <Segment>
+                                <Form className="form-filtros">
+                                    
+                                    <Form.Input
+                                        icon="search"
+                                        value={codigo}
+                                        onChange={e => handleChangeCodigo(e.target.value)}
+                                        label='Código do Produto'
+                                        placeholder='Filtrar por Código do Produto'
+                                        labelPosition='left'
+                                        width={4}
+                                    />
+                                             <Form.Group widths='equal'>                   
+                <Form.Input
+                    icon="search"
+                    value={titulo}
+                    onChange={e => handleChangeTitulo(e.target.value)}
+                    label='Título'
+                    placeholder='Filtrar por título'
+                    labelPosition='left'
+                />
+                <Form.Select
+                    placeholder='Filtrar por Categoria'
+                    label='Categoria'
+                    options={listaCategoriaProduto}
+                    value={idCategoria}
+                    onChange={(e,{value}) => {
+  handleChangeCategoriaProduto(value)
+                                           }}
+                />
+                
+            </Form.Group>
+        </Form>
+    </Segment>:""
+}
+
+
+                        
+
                         <br /><br /><br />
 
                         <Table color='orange' sortable celled>
