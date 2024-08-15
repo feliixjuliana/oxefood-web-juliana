4a8aeb06f769302787f4f60b103da2cc01f8274e
aluno <aluno@ifpe>
2024-08-15 16:30:03 -0300

    front

--- src/MenuSistema.jsx
+++ src/MenuSistema.jsx
@@ -1,73 +1,78 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Menu } from "semantic-ui-react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
+import { logout } from './views/util/AuthenticationService';
 
-class MenuSistema extends React.Component{
+class MenuSistema extends React.Component {
+  state = {
+    activeItem: 'home',
+  };
 
-   state = {
-       activeItem: 'home'
-   }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
-   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  render() {
+    return (
+      <>
+        <Menu inverted>
+          <Menu.Item
+            name="home"
+            active={this.state.activeItem === 'home'}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/"
+          />
 
-   render(){
-       return(
-           <>
-               <Menu inverted>
-                  
-                   <Menu.Item
-                       name='home'
-                       active={this.state.activeItem === 'home'}
-                       onClick={this.handleItemClick}
-                       as={Link}
-                       to='/'
-                   />
+          <Menu.Item
+            name="cliente"
+            active={this.state.activeItem === 'cliente'}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/list-cliente"
+          />
 
-                    <Menu.Item
-                       name='cliente'
-                       active={this.state.activeItem === 'cliente'}
-                       onClick={this.handleItemClick}
-                       as={Link}
-                       to='/list-cliente'
-                   />
+          <Menu.Item
+            name="produto"
+            active={this.state.activeItem === 'produto'}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/list-produto"
+          />
 
-                   <Menu.Item
-                       name='produto'
-                       active={this.state.activeItem === 'produto'}
-                       onClick={this.handleItemClick}
-                       as={Link}
-                       to='/list-produto'
-                   />
+          <Menu.Item
+            name="entregador"
+            active={this.state.activeItem === 'entregador'}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/list-entregador"
+          />
 
-                   <Menu.Item
-                       name='entregador'
-                       active={this.state.activeItem === 'entregador'}
-                       onClick={this.handleItemClick}
-                       as={Link}
-                       to='/list-entregador'
-                   />
+          <Menu.Item
+            name="fornecedor"
+            active={this.state.activeItem === 'fornecedor'}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/list-fornecedor"
+          />
 
-                       <Menu.Item
-                       name='fornecedor'
-                       active={this.state.activeItem === 'fornecedor'}
-                       onClick={this.handleItemClick}
-                       as={Link}
-                       to='/list-fornecedor'
-                   />
+          <Menu.Item
+            name="fabricante"
+            active={this.state.activeItem === 'fabricante'}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/list-fabricante"
+          />
 
-                    <Menu.Item
-                       name='fabricante'
-                       active={this.state.activeItem === 'fabricante'}
-                       onClick={this.handleItemClick}
-                       as={Link}
-                       to='/list-fabricante'
-                   />
-
-
-               </Menu>
-           </>
-       )
-   }
+          <Menu.Item
+            className="navbar__item--mobile"
+            onClick={logout}
+            content="Sair"
+            as={Link}
+            to="/"
+          />
+        </Menu>
+      </>
+    );
+  }
 }
 
 export default MenuSistema;
--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -3,34 +3,64 @@ import { Route, Routes } from "react-router-dom";
 
 import FormCliente from './views/cliente/FormCliente';
 import ListCliente from './views/cliente/ListCliente';
-import FormEntregador from './views/entregador/FormEntregador';
-import ListEntregador from './views/entregador/ListEntregador';
 import Home from './views/home/Home';
+import FormLogin from './views/login/FormLogin';
 import FormProduto from './views/produto/FormProduto';
 import ListProduto from './views/produto/ListProduto';
-import FormFornecedor from './views/fornecedor/FormFornecedor';
-import ListFornecedor from './views/fornecedor/ListFornecedor';
-import FormFabricante from './views/fabricante/FormFabricante';
-import ListFabricante from './views/fabricante/ListFabricante';
-
+import { ProtectedRoute } from './views/util/ProtectedRoute';
 
 function Rotas() {
     return (
-        <>
-            <Routes>
-                <Route path="/" element={ <Home/> } />
-                <Route path="form-cliente" element={ <FormCliente/> } />
-                <Route path="list-cliente" element={ <ListCliente/> } />
-                <Route path="form-produto" element={ <FormProduto/> } />
-                <Route path="list-produto" element={ <ListProduto/> } />
-                <Route path="form-entregador" element={ <FormEntregador/> } />
-                <Route path="list-entregador" element={ <ListEntregador/> } />
-                <Route path="form-fornecedor" element={ <FormFornecedor/> } />
-                <Route path="list-fornecedor" element={ <ListFornecedor/> } />
-                <Route path="list-fabricante" element={ <ListFabricante/> } />
-                <Route path="form-fabricante" element={ <FormFabricante/> } />
-            </Routes>
-        </>
+        <Routes>
+            
+            <Route path="/" element={ <FormLogin/> } />
+
+            <Route
+                path="/home"
+                element={
+                <ProtectedRoute>
+                    <Home />
+                </ProtectedRoute>
+                }
+            />
+                
+            <Route
+                path="/list-cliente"
+                element={
+                    <ProtectedRoute>
+                        <ListCliente />
+                    </ProtectedRoute>
+                }
+            />
+
+            <Route
+                path="/form-cliente"
+                element={
+                <ProtectedRoute>
+                    <FormCliente />
+                </ProtectedRoute>
+                }
+            />
+
+            <Route
+                path="/list-produto"
+                element={
+                <ProtectedRoute>
+                    <ListProduto />
+                </ProtectedRoute>
+                }
+            />
+
+            <Route
+                path="/form-produto"
+                element={
+                <ProtectedRoute>
+                    <FormProduto />
+                </ProtectedRoute>
+                }
+            />
+
+        </Routes>
     )
 }
 
